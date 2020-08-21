7faf96f6347730e575b777ff8c9ca47ce85485c9
chan-p <hyshtmnr6239dcm@gmail.com>
2020-08-22 01:07:36 +0900

    add calendar

--- dairy-ts/src/App.tsx
+++ dairy-ts/src/App.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import { Diary } from './Diary'
+// import { Diary } from './Diary'
+import Calendar from 'react-calendar'
 import './App.css';
 
-const initialState: Diary[] = [
-  {
-      id: 1,
-      date: '2020-8-22',
-      title: '調子がよかった'
-  }
-]
+// const initialState: Diary[] = [
+//   {
+//       id: 1,
+//       date: '2020-8-22',
+//       title: '調子がよかった'
+//   }
+// ]
 
 const App: React.FC = () => {
 
   return (
-      <div>
-          <input type="date"/>
-      </div>
+    <div>
+      <Calendar locale="ja-JP" calendarType="US" value={new Date()} />
+    </div>
   )
 }
 
